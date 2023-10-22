1a9b5ab1ead3c40e67ef660a7f973efaa2dcf4a2
Andon Mefford <andonmefford@outlook.com>
2023-10-22 16:59:25 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    const distances = new Array(graph.length);
+    const visited = new Array(graph.length);
+    for (let i = 0; i < graph.length; i++) {
+        distances[i] = Infinity;
+        visited[i] = false;
+    }
+    distances[sourceNode] = 0;
+    for (let i = 0; i < graph.length - 1; i++) {
+        let minDistance = Infinity;
+        let minNode = -1;
+        for (let j = 0; j < graph.length; j++) {
+            if (!visited[j] && distances[j] < minDistance) {
+                minDistance = distances[j];
+                minNode = j;
+            }
+        }
+        if (minNode === -1) {
+            break;
+        }
+        visited[minNode] = true;
+        for (let neighbor = 0; neighbor < graph.length; neighbor++) {
+            if (!visited[neighbor] && graph[minNode][neighbor] !== 0) {
+                const weight = graph[minNode][neighbor];
+                const newDistance = distances[minNode] + weight;
+                if (newDistance < distances[neighbor]) {
+                    distances[neighbor] = newDistance;
+                }
+            }
+        }
+    }
+    return distances;
 }
