70940248a2559f153e2d1abcefde23dcd2d96046
Andon Mefford <andonmefford@outlook.com>
2023-12-10 16:41:36 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,29 +3,36 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-const graph = [
+function testDijkstra(graph, sourceNode, expectedDistances) {
+    const result = dijkstra(graph, sourceNode);
+    for (let i = 0; i < result.length; i++) {
+        assert.equal(result[i], expectedDistances[i]);
+    }
+}
+
+const connectedGraph = [
     [0, 2, 4, 0, 0],
     [0, 0, 1, 7, 0],
     [0, 0, 0, 2, 0],
     [0, 0, 0, 0, 3],
     [0, 0, 0, 0, 0]
 ];
-assert(JSON.stringify(dijkstra(graph)) == (0, [0, 2, 3, 9, 6]);
+testDijkstra(connectedGraph, 0, [0, 2, 3, 9, 6]);
 
-const graph = [
+const disconnectedGraph = [
     [0, 2, 0, 0, 0],
     [0, 0, 0, 7, 0],
     [0, 0, 0, 0, 2],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0]
 ];
-assert(JSON.stringify(dijkstra(graph)) == (0, [0, 2, Infinity, Infinity, Infinity]);
+testDijkstra(disconnectedGraph, 0, [0, 2, Infinity, Infinity, Infinity]);
 
-const graph = [
+const negativeWeightGraph = [
     [0, 2, -1, 0, 0],
     [0, 0, 1, 7, 0],
     [0, 0, 0, 2, 0],
     [0, 0, 0, 0, 3],
     [0, 0, 0, 0, 0]
 ];
-assert(JSON.stringify(dijkstra(graph)) == (0, [0, 2, -1, 0, 0]);
+testDijkstra(negativeWeightGraph, 0, [0, 2, -1, 0, 0]);
